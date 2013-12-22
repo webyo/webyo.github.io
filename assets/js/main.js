88ba9eaaa7b20df1b43eb328e4f5a5ee7a0bff8b
webyo <eric@webyo.es>
2013-12-22 23:30:00 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 (function($) {
 
 	$(document).ready( function() {
@@ -34,59 +33,7 @@
 			
 		}
 
-		// form validation 
-		Modernizr.load({
-			test: Modernizr.input.autocomplete,		
-			nope: ['assets/js/jquery.validate.js', 'assets/js/jquery.validate.bootstrap.js'],
-		});
-
-		// ajax contact form
-		$('.contact-form form').submit( function(e) {
-			
-			e.preventDefault();
-
-			$theForm = $(this);
-			$btn = $(this).find('#submit-button');
-			$alert = $(this).parent().find('.alert');			
-
-			// just to check if validation supported without response, such as safari 5.1. Removing JS error on chrome
-			if( !Modernizr.input.autocomplete ) {
-				
-				$theForm.validate({
-
-					messages: {
-						email: { required: "Email is required", email: "Please enter a valid email address"}
-					}
-				});	
-
-				if( !$theForm.valid() ) {
-					return;
-				}
-			}
-
-			$btn.addClass('loading');
-			$btn.attr('disabled', 'disabled');
-
-			$.post('contact.php', $(this).serialize(), function(data){
-				
-				$message = data.message;
-				
-				if( data.result == true ){
-					$theForm.slideUp('medium', function() {
-						$alert.removeClass('alert-danger');
-						$alert.addClass('alert-success').html($message).slideDown('medium');	
-					});				
-				}else {
-					$alert.addClass('alert-danger').html($message).slideDown('medium');	
-				}
-
-				$btn.removeClass('loading');
-				$btn.removeAttr('disabled');
-
-			})
-			.fail(function() { console.log('AJAX Error'); });
-
-		});
+		
 
 	});
 
